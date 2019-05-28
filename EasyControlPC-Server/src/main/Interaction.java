10738a543040da24323703b1d8d681f0f194b8f8
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-05-28 21:27:12 +0800

    整理代码

--- EasyControlPC-Server/src/main/Interaction.java
+++ EasyControlPC-Server/src/main/Interaction.java
@@ -10,27 +10,27 @@ import java.awt.event.KeyEvent;
 public class Interaction {
 
 	private Robot robot;
-	
+
 	public Interaction() {
 		try {
-			robot=new Robot();
-			
+			robot = new Robot();
+
 		} catch (AWTException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void move(int x,int y) {
+
+	public void move(int x, int y) {
 		PointerInfo pinfo = MouseInfo.getPointerInfo();
 		Point p = pinfo.getLocation();
-		int now_x = (int)p.getX();
-		int now_y = (int)p.getY();
-		robot.mouseMove(now_x+x, now_y+y);
+		int now_x = (int) p.getX();
+		int now_y = (int) p.getY();
+		robot.mouseMove(now_x + x, now_y + y);
 	}
-	
+
 	public void click() {
 		robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
 		robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
 	}
-	
+
 }
--- EasyControlPC-Server/src/main/Server.java
+++ EasyControlPC-Server/src/main/Server.java
@@ -3,7 +3,6 @@ package main;
 import java.io.*;
 import java.net.*;
 
-
 public class Server {
 	private ServerSocket serverSocket;
 	private Socket server;
@@ -11,13 +10,13 @@ public class Server {
 	private PrintWriter out;
 	private MessageBean msgBean;
 	private Interaction interaction;
-	
+
 	public Server() {
-		msgBean=new MessageBean();
-		interaction=new Interaction();
-		
+		msgBean = new MessageBean();
+		interaction = new Interaction();
+
 		try {
-			while(true) {
+			while (true) {
 				serverSocket = new ServerSocket(8888);
 				System.out.println("等待建立连接");
 				server = serverSocket.accept();
@@ -29,69 +28,73 @@ public class Server {
 				out = new PrintWriter(server.getOutputStream(), false);
 				System.out.println("客户端上线：" + remoteIP + ":" + remotePort);
 				System.out.println();
-				//发送连接成功消息
-				sendMsg(new MessageBean(1,"欢迎使用EasyControlPC",0,0));
-				
+				// 发送连接成功消息
+				sendMsg(new MessageBean(1, "欢迎使用EasyControlPC", 0, 0));
+
 				// 接收客户端消息
-				while(true) {
-					msgBean=receiveMsg();
-					if(msgBean!=null) {
-						System.out.println("接收到消息："+msgBean.getMessage());
-						
-						if(msgBean.getState()==-1) {
+				while (true) {
+					msgBean = receiveMsg();
+					if (msgBean != null) {
+						System.out.println("接收到消息：" + msgBean.getMessage());
+
+						if (msgBean.getState() == -1) {
 							System.out.println("客户端退出：" + remoteIP + ":" + remotePort);
 							System.out.println();
 							close();
 							break;
 						}
-						
-						if(msgBean.getState()==4) {
+
+						if (msgBean.getState() == 4) {
 							interaction.move(msgBean.getMoveX(), msgBean.getMoveY());
 						}
-						
-						else if(msgBean.getState()==5) {
+
+						else if (msgBean.getState() == 5) {
 							interaction.click();
 						}
 
-						sendMsg(new MessageBean(2,"服务端已接收！",0,0));
-					}
-					else {
+						sendMsg(new MessageBean(2, "服务端已接收！", 0, 0));
+					} else {
 						System.out.println("客户端退出：" + remoteIP + ":" + remotePort);
 						System.out.println();
 						close();
 						break;
 					}
-					
+
 				}
 			}
-			
+
 		} catch (Exception e) {
 			System.out.println("失去连接！");
 		}
-		
+
 	}
-	
+
 	public void sendMsg(MessageBean msg) {
-		if(out!=null) {
+		if (out != null) {
 			out.println(msg.toString());
 			out.flush();
 		}
 	}
-	
+
 	public MessageBean receiveMsg() throws IOException {
-		if(in!=null) {
-			String data=in.readLine();
-			if(data!=null) return new MessageBean(data);
+		if (in != null) {
+			String data = in.readLine();
+			if (data != null)
+				return new MessageBean(data);
 			return null;
 		}
 		return null;
 	}
-	
+
 	public void close() throws IOException {
-		if(out!=null) out.close();
-		if(in!=null) in.close();
-		if(server!=null) server.close();
-		if(serverSocket!=null) serverSocket.close();
+		if (out != null)
+			out.close();
+		if (in != null)
+			in.close();
+		if (server != null)
+			server.close();
+		if (serverSocket != null)
+			serverSocket.close();
 	}
 
 	public static void main(String[] args) {
